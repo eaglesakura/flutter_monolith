e9bf170c226978b428ea605773806b7bdf167621
eaglesakura <eagle.sakura@gmail.com>
2025-07-11 18:47:47 +0900

    chg: ログ出力の色を指定

--- monolith_runner/lib/src/monolith_runner_arguments.dart
+++ monolith_runner/lib/src/monolith_runner_arguments.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: avoid_print
+
+import 'package:ansicolor/ansicolor.dart';
+import 'package:armyknife_logger/armyknife_logger.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:monolith/monolith.dart';
 
@@ -14,10 +18,34 @@ abstract class MonolithRunnerArguments with _$MonolithRunnerArguments {
   }) = _MonolithRunnerArguments;
 
   factory MonolithRunnerArguments.configure(List<String> arguments) {
+    _injectLogger();
     return MonolithRunnerArguments(
       monolith: Monolith(
         const MonolithOptions(),
       ),
     );
+  }
+
+  static void _injectLogger() {
+    final redPen = AnsiPen()..red(bold: true);
+    final yellowPen = AnsiPen()..yellow(bold: true);
+    final bluePen = AnsiPen()..blue(bold: true);
+
+    LoggerProxy.i = (String tag, String message) {
+      print('${bluePen('[$tag]')} $message');
+    };
+
+    LoggerProxy.w = (String tag, String message) {
+      print(yellowPen('[$tag] $message'));
+    };
+    LoggerProxy.e =
+        (String tag, String message, [dynamic error, StackTrace? stackTrace]) {
+          print(redPen('[$tag] $message'));
+          print(redPen(error.toString()));
+          print(redPen(stackTrace.toString()));
+        };
+    LoggerProxy.d = (String tag, String message) {
+      print(yellowPen('[$tag] $message'));
+    };
   }
 }
